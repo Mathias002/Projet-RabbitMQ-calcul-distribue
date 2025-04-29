77bcdcc69903caf03a076d4bcbeafe755074a728
Mathias002 <moussetmathias@gmail.com>
2025-04-29 23:47:17 +0200

    Review code + minor edit
    Ajout préfixe 002_ pour nos queue afin de les différencier sur le serveur / Ajout d'un index pour identifier les calculs / Saut de ligne dans le terminal avec des console.log('') / Suppression des Workers et du consumer si Ctrl C dans le terminal / Ajout modif commentaires

--- workers/worker_1.js
+++ workers/worker_1.js
@@ -1,13 +1,19 @@
-const amqlib = require('amqplib');
+const ampqlib = require('amqplib');
+require('dotenv').config();
 
-const rabbitmq_url = 'amqp://user:password@infoexpertise.hopto.org:5679'
+const serverCredentials = process.env.SERVER_CREDENTIALS;
+
+const rabbitmq_url = 'amqp://' + serverCredentials;
 
 // Récupération de l'op dans la comande du terminal 
 // "add", "sub", "mul", "div"
 const op = process.argv[2];
 
 // Initialisation d'une queue selon l'opération reçu
-const queue = `calc_${op}`;
+const queue = `002_calc_${op}`;
+
+// Initialisation d'une variable channel global
+let channel;
 
 // Vérification de l'opération dans les paramètres de la commande
 if(!["add", "sub", "div", "mul"].includes(op)){
@@ -18,36 +24,40 @@ if(!["add", "sub", "div", "mul"].includes(op)){
 async function playWorker(){
 
     // Connexion à rabbitmq
-    const connection = await amqlib.connect(rabbitmq_url);
+    const connection = await ampqlib.connect(rabbitmq_url);
 
     // Création du channel
-    const channel = await connection.createChannel();
+    channel = await connection.createChannel();
 
     // Assertion de la queue requestQueue
     channel.assertQueue(queue, {
         durable: false
     });
 
-    // Assertion de la queue resultQueue
-    channel.assertQueue(resultQueue, {
-        durable: false
-    });
+    // Suppression de la queue à l'arrêt
+    process.on('SIGINT', async () => {
+        await channel.cancel(queue)
+        await channel.deleteQueue(queue);
+        console.log(`Worker arrêter pour l'opération ${op}`);
+        process.exit(0);
+    })
 
     console.log(`Worker prêt pour l'opération ${op}`);
     
     // Reception du message 
-    channel.consume(requestQueue, consume);
+    channel.consume(queue, consume);
 }
 
 async function consume(message){
     if(message != null){
         
         // Récupération des opérands et du type d'opération
-        const { n1, n2, op: operation } = JSON.parse(message.content.toString());
+        const { index, n1, n2, op: operation } = JSON.parse(message.content.toString());
 
         // Mise en place du délai de calcul entre 5-15 secondes
         const delay = Math.floor(Math.random() * 11) + 5;
-        console.log(`[${op}] Calcul en cours de traitement : (${n1} ${operation} ${n2}) | Attente: ${delay}s`);
+        console.log(`[${op}] Calcul numéro [${index}] en cours de traitement : (${n1} ${operation} ${n2}) | Attente: ${delay}s`);
+        console.log('');
         await new Promise(res=> setTimeout(res, delay * 1000));
 
         // Switch case sur l'opération et calcul en fonction 
@@ -60,13 +70,14 @@ async function consume(message){
         }
 
         // Initialisation du résultat 
-        const resultJSON = { n1, n2, op, result };
+        const resultJSON = { index, n1, n2, op, result };
 
         // Envoie du resultat à la queue sous forme de JSON
-        channel.sendToQueue('cals_results', Buffer.from(JSON.stringify(resultJSON)));
+        channel.sendToQueue('002_calc_results', Buffer.from(JSON.stringify(resultJSON)));
         
-        // 
-        console.log(`[${op}] Résultat envoyé: `, resultJSON);
+        // On display le resultat en console puis on envoie un accusé de reception à la queue
+        console.log(`[${op}] Résultat du calcul [${index}] envoyé: `, resultJSON);
+        console.log('');
         channel.ack(message);
     }
 }
